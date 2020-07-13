8b07bb923dc47fd9dd516919743ff0cc231dec33
Wassime Desktop <60438665+Spitfire0016@users.noreply.github.com>
2020-07-13 04:06:39 +0100

    good new alas
    -added device type to data generation
    -device page (some comments need to be uncommented)
    -temperature page (also uncomment in controller)
    -lighting page (also this)
    -security page (and finally this one too)
    -cameras are 0% loading

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -11,7 +11,7 @@ class DeviceController extends AbstractController
     /**
      * @Route("/device/{paramRoom?all}", name="device")
      */
-    public function index($paramRoom)
+    public function Devices($paramRoom)
     {
         /*$user = $this->getUser();
         $repository = $this->getDoctrine()->getRepository(User::class);
@@ -29,11 +29,85 @@ class DeviceController extends AbstractController
     }
 
     /**
+     * @Route("/lighting", name="devicelight")
+     */
+    public function Lighting()
+    {
+        /*$user = $this->getUser();
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $me = $repository->findOneBy(['username' => $user->getUsername()]);*/
+        $repository = $this->getDoctrine()->getRepository('App:SmartHouse');
+        /*$house = $repository->find($me->getHouseID());*/ $house = $repository->find(4); //<--- delete this and uncomment the rest
+            $rooms = $house->getRooms();
+            foreach ($rooms as $room) {
+                foreach($room->getDevices() as $device){
+                    if($device->getType()!="lighting")
+                        $room->getDevices()->removeElement($device);
+                }
+                if($room->getDevices()->isEmpty())
+                    $rooms->removeElement($room);
+            }
+        return $this->render('device/index.html.twig', [
+            'rooms' => $rooms
+        ]);
+    }
+
+    /**
+     * @Route("/temperature", name="devicetemp")
+     */
+    public function temperature()
+    {
+        /*$user = $this->getUser();
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $me = $repository->findOneBy(['username' => $user->getUsername()]);*/
+        $repository = $this->getDoctrine()->getRepository('App:SmartHouse');
+        /*$house = $repository->find($me->getHouseID());*/ $house = $repository->find(4); //<--- delete this and uncomment the rest
+            $rooms = $house->getRooms();
+            foreach ($rooms as $room) {
+                foreach($room->getDevices() as $device){
+                    if($device->getType()!="temperature")
+                        $room->getDevices()->removeElement($device);
+                }
+                if($room->getDevices()->isEmpty())
+                    $rooms->removeElement($room);
+            }
+            
+        return $this->render('device/index.html.twig', [
+            'rooms' => $rooms
+        ]);
+    }
+
+    /**
+     * @Route("/security", name="devicesecure")
+     */
+    public function security()
+    {
+        /*$user = $this->getUser();
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $me = $repository->findOneBy(['username' => $user->getUsername()]);*/
+        $repository = $this->getDoctrine()->getRepository('App:SmartHouse');
+        /*$house = $repository->find($me->getHouseID());*/ $house = $repository->find(4); //<--- delete this and uncomment the rest
+            $rooms = $house->getRooms();
+            foreach ($rooms as $room) {
+                foreach($room->getDevices() as $device){
+                    if($device->getType()!="security")
+                        $room->getDevices()->removeElement($device);
+                }
+                if($room->getDevices()->isEmpty())
+                    $rooms->removeElement($room);
+            }
+            
+        return $this->render('device/index.html.twig', [
+            'rooms' => $rooms
+        ]);
+    }
+
+    /**
      * @Route("/DeviceAll/{paramHouse?1}/{paramRoom?all}", name="deviceDebugAll")
      */
     public function GodMode($paramHouse,$paramRoom)
     {
-        
+        //used to simulate the actual devices, can access anything anywhere
         $repository = $this->getDoctrine()->getRepository('App:SmartHouse');
         $house = $repository->find($paramHouse);
         if($paramRoom == 'all'){
